f9448f711afa5177e6bfd36c21812edc3fce61c3
Ngoc An Nguyen <andyannguyen08@gmail.com>
2025-12-12 14:30:00 -0800

    Rube Goldberg.ino

--- Rube Goldberg.ino
+++ Rube Goldberg.ino
@@ -1,33 +1,64 @@
+//DC motor
 const int motor1 = 6;
-const int motor2 = 3;
+const int motor2 = 5;
 
-const int trig = 10;
-const int echo = 11;
+//First sensor to detect the team before us
+const int trig1 = 10;
+const int echo1 = 11;
 
-long duration;
+//Second sensor to detect our marbale
+const int trig2 = 3;
+const int echo2 = 2;
 
-long distance;
+//Time that the first sensor detects something
+long duration1;
+
+//Distance from the first sensor
+long distance1;
+
+//Time that the second sensor detects something
+long duration2;
+
+//Distance from the second sensor
+long distance2;
+
+//Buzzer
+const int buzzer = 9;
+//************************************//
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode (motor1, OUTPUT);
   pinMode (motor2, OUTPUT);
-  pinMode (trig, OUTPUT);
-  pinMode (echo, INPUT);
+
+  pinMode (trig1, OUTPUT);
+  pinMode (echo1, INPUT);
+
+  pinMode (trig2, OUTPUT);
+  pinMode (echo2, INPUT);
+
+  pinMode (buzzer, OUTPUT);
+  Serial.begin (9600);
 }
 
+//********************************************//
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(trig, LOW);
+  //Code for the first sensor
+  digitalWrite(trig1, LOW);
   delayMicroseconds(2);
 
-  digitalWrite(trig, HIGH);
+  digitalWrite(trig1, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trig, LOW);
+  digitalWrite(trig1, LOW);
 
-  duration = pulseIn(echo, HIGH);
-  distance = duration * 0.034 / 2;
+  duration1 = pulseIn(echo1, HIGH);
+  distance1 = duration1 * 0.034 / 2;
 
-  if (distance <= 5)
+  Serial.print ("Distance from the first sensor: ");
+  Serial.println (distance1);
+
+  //Code for the DC motor
+  if (distance1 <= 5)
   {
     digitalWrite (motor1, HIGH);
     digitalWrite (motor2, LOW);
@@ -36,5 +67,34 @@ void loop() {
   {
     digitalWrite (motor1, LOW);
     digitalWrite (motor2, LOW);
+  }
+  
+  //Code for the second sensor
+  digitalWrite(trig2, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trig2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig2, LOW);
+
+  duration2 = pulseIn(echo2, HIGH);
+  distance2 = duration2 * 0.034 / 2;
+
+  Serial.print ("Distance from the second sensor: ");
+  Serial.println (distance2);
+
+  //Code for the buzzer
+  if (distance2 <= 5)
+  {
+    tone (buzzer, 131, 100);
+    //delay (100);
+    tone (buzzer, 147, 100);
+    //delay (100);
+    tone (buzzer, 165, 100);
+    //delay (100);
+  }
+  else 
+  {
+    tone (buzzer, 0, 0);
   }
 }
